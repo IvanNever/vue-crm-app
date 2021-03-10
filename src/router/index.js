21928e5632321238f56c34a2f6ce67d4a2239506
Ivan Neverovsky <ivan@neverovsky.com.ua>
2021-03-10 18:59:28 +0200

    layout and page templates

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,62 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "HomePage",
+    meta: { layout: "main" },
+    component: () => import("@/views/HomePage.vue")
+  },
+  {
+    path: "/login",
+    name: "LoginPage",
+    meta: { layout: "empty" },
+    component: () => import("@/views/LoginPage.vue")
+  },
+  {
+    path: "/register",
+    name: "RegisterPage",
+    meta: { layout: "empty" },
+    component: () => import("@/views/RegisterPage.vue")
+  },
+  {
+    path: "/categories",
+    name: "CategoriesPage",
+    meta: { layout: "main" },
+    component: () => import("@/views/CategoriesPage.vue")
+  },
+  {
+    path: "/detail-record",
+    name: "DetailRecordPage",
+    meta: { layout: "main" },
+    component: () => import("@/views/DetailRecordPage.vue")
+  },
+  {
+    path: "/history",
+    name: "HistoryPage",
+    meta: { layout: "main" },
+    component: () => import("@/views/HistoryPage.vue")
+  },
+  {
+    path: "/planning",
+    name: "PlanningPage",
+    meta: { layout: "main" },
+    component: () => import("@/views/PlanningPage.vue")
+  },
+  {
+    path: "/profile",
+    name: "ProfilePage",
+    meta: { layout: "main" },
+    component: () => import("@/views/ProfilePage.vue")
+  },
+  {
+    path: "/record",
+    name: "RecordPage",
+    meta: { layout: "main" },
+    component: () => import("@/views/RecordPage.vue")
   }
 ];
 
