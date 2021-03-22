9db18d1d64565e9337a78b6676c98d169566f4bb
Ivan Neverovsky <ivan@neverovsky.com.ua>
2021-03-22 11:07:23 +0200

    pagination in history page

--- src/views/HistoryPage.vue
+++ src/views/HistoryPage.vue
@@ -13,37 +13,49 @@
     <p class="center" v-else-if="!records.length">У вас еще нет записей</p>
 
     <section v-else>
-      <HistoryTable :records="records" />
+      <HistoryTable :records="items" />
+      <Paginate
+        v-model="page"
+        :page-count="pageCount"
+        :click-handler="pageChangeHandler"
+        :prev-text="'Назад'"
+        :next-text="'Вперед'"
+        :container-class="'pagination'"
+        :page-class="'waves-effect'"
+      />
     </section>
   </div>
 </template>
 
 <script>
 import HistoryTable from "@/components/HistoryTable";
+import paginationMixin from "@/mixins/pagination.mixin";
 
 export default {
   name: "HistoryPage",
+  mixins: [paginationMixin],
   components: {
     HistoryTable
   },
   data: () => ({
     loading: true,
-    records: [],
-    categories: []
+    records: []
   }),
   async mounted() {
-    this.categories = await this.$store.dispatch("fetchCategories");
-    const records = await this.$store.dispatch("fetchRecords");
-    this.records = records.map(record => {
-      return {
-        ...record,
-        categoryName: this.categories.find(
-          category => category.id === record.categoryId
-        ).name,
-        typeClass: record.type === "income" ? "green" : "red",
-        typeText: record.type === "income" ? "Доход" : "Расход"
-      };
-    });
+    const categories = await this.$store.dispatch("fetchCategories");
+    this.records = await this.$store.dispatch("fetchRecords");
+    this.setupPagination(
+      this.records.map(record => {
+        return {
+          ...record,
+          categoryName: categories.find(
+            category => category.id === record.categoryId
+          ).name,
+          typeClass: record.type === "income" ? "green" : "red",
+          typeText: record.type === "income" ? "Доход" : "Расход"
+        };
+      })
+    );
 
     this.loading = false;
   }
