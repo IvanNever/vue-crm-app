c49d8f0d06056f037929d8c0b7c2d32a15ed3c85
Ivan Neverovsky <ivan@neverovsky.com.ua>
2021-03-22 12:14:34 +0200

    chart in history page

--- src/views/HistoryPage.vue
+++ src/views/HistoryPage.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <canvas ref="canvas"></canvas>
     </div>
 
     <Loader v-if="loading" />
@@ -30,34 +30,77 @@
 <script>
 import HistoryTable from "@/components/HistoryTable";
 import paginationMixin from "@/mixins/pagination.mixin";
+import { Pie } from "vue-chartjs";
 
 export default {
   name: "HistoryPage",
+  extends: Pie,
   mixins: [paginationMixin],
   components: {
     HistoryTable
   },
   data: () => ({
     loading: true,
-    records: []
+    records: [],
+    categories: []
   }),
   async mounted() {
-    const categories = await this.$store.dispatch("fetchCategories");
+    this.categories = await this.$store.dispatch("fetchCategories");
     this.records = await this.$store.dispatch("fetchRecords");
-    this.setupPagination(
-      this.records.map(record => {
-        return {
-          ...record,
-          categoryName: categories.find(
-            category => category.id === record.categoryId
-          ).name,
-          typeClass: record.type === "income" ? "green" : "red",
-          typeText: record.type === "income" ? "Доход" : "Расход"
-        };
-      })
-    );
+
+    this.setup();
 
     this.loading = false;
+  },
+  methods: {
+    setup() {
+      this.setupPagination(
+        this.records.map(record => {
+          return {
+            ...record,
+            categoryName: this.categories.find(
+              category => category.id === record.categoryId
+            ).name,
+            typeClass: record.type === "income" ? "green" : "red",
+            typeText: record.type === "income" ? "Доход" : "Расход"
+          };
+        })
+      );
+
+      this.renderChart({
+        labels: this.categories.map(category => category.name),
+        datasets: [
+          {
+            label: "Расходы по категориям",
+            data: this.categories.map(category => {
+              return this.records
+                .filter(
+                  item =>
+                    item.categoryId === category.id && item.type === "outcome"
+                )
+                .reduce((total, record) => (total += +record.amount), 0);
+            }),
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      });
+    }
   }
 };
 </script>
